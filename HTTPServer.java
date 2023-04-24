fd1cda45863cba69c8a65f07781187a8e342a6d4
JJosan <nosajn123@gmail.com>
2023-04-24 04:55:05 -0700

    no more external libraries

--- HTTPServer.java
+++ HTTPServer.java
@@ -1,11 +1,9 @@
 import java.io.*;
-import java.net.InetSocketAddress;
-import java.nio.file.*;
+import java.net.*;
 import java.util.*;
-import com.sun.net.httpserver.*;
 
 public class HTTPServer {
-
+    private static final int PORT = 8080;
     private static final String FILE_ROOT = "resources";
     private static final HashMap<String, String> SUPPORTED_MIME_TYPES = new HashMap<>(){{
         put("txt", "text/plain");
@@ -13,148 +11,208 @@ public class HTTPServer {
         put("json", "text/json");
     }};
 
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-        server.createContext("/", new Handler());
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Server is listening on port 8080");
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(PORT);
+        System.out.println("Server connect to port: " + PORT);
+        while(true) {
+            try {
+                Socket socket = server.accept();
+                InputStream in = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                String firstLine = reader.readLine();
+
+                String body = "";
+                String requestMethod = "";
+                String urlPath = "";
+                String extension = "";
+
+                if (firstLine != null) {
+                    // get headers
+                    requestMethod = firstLine.split(" ", 3)[0];
+                    urlPath = firstLine.split(" ", 3)[1];
+
+                    // check for valid mime
+                    String[] temp = urlPath.split("\\.");
+                    if (temp.length != 2) {
+                        unsupportedMediaType(socket);
+                        continue;
+                    }
+                    extension = temp[1];
+                    if (!SUPPORTED_MIME_TYPES.containsKey(extension)) {
+                        unsupportedMediaType(socket);
+                        continue;
+                    }
+
+                    // get body
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains("Content-Length")) break;
+                    }
+                    int cLength = Integer.valueOf(line.split(" ")[1]);
+                    reader.readLine();
+                    for (int i = 0, c = 0; i < cLength; i++) {
+                        c = reader.read();
+                        body += (char)c;
+                    }
+                }
+
+                switch(requestMethod) {
+                    case "GET":
+                        handleGet(socket, FILE_ROOT+urlPath);
+                        break;
+                    case "POST":
+                        handlePost(socket, FILE_ROOT+urlPath, extension, body);
+                        break;
+                    case "PUT":
+                        handlePut(socket, FILE_ROOT+urlPath, body);
+                        break;
+                    case "DELETE":
+                        handleDelete(socket, FILE_ROOT+urlPath);
+                        break;
+                    default:
+                        break;
+                }
+                reader.close();
+            } catch(IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
-    private static class Handler implements HttpHandler {
-        
-        @Override
-        public void handle(HttpExchange t) throws IOException {
-            // get urls and stuff
-            String urlPath = t.getRequestURI().getPath();
-            Path filePath = Paths.get(FILE_ROOT + urlPath);
-            File file = filePath.toFile();
-            
-            // get file information
-            String[] temp = urlPath.split("\\.");
-            if (temp.length != 2) {
-                t.sendResponseHeaders(415, 0);
-                return;
-            }
-            String extension = temp[1];
 
-            String requestMethod = t.getRequestMethod();
-            switch(requestMethod) {
-                case "GET":
-                    if (!fileExists(t, file)) break;
-                    // check if file extension is supported
-                    if (!SUPPORTED_MIME_TYPES.containsKey(extension)) {
-                        t.sendResponseHeaders(415, 0);
-                        break;
-                    }
-                    send(t, filePath);
-                    break;
-                case "POST":
-                    if (!fileExists(t, file)) break;
-                    // check if file to be editted is in plain text
-                    if (!mime(filePath).equals("text/plain")) {
-                        t.sendResponseHeaders(415, 0);
-                        break;
-                    }
-                    editFile(t, FILE_ROOT + urlPath);
-                    send(t, filePath);
-                    break;
-                case "PUT":
-                    // check if file extension is supported
-                    if (!SUPPORTED_MIME_TYPES.containsKey(extension)) {
-                        t.sendResponseHeaders(415, 0);
-                        break;
-                    }
-                    overrideFile(t, FILE_ROOT + urlPath);
-                    send(t, filePath);
-                    break;
-                case "DELETE":
-                    if (!fileExists(t, file)) break;
-                    deleteFile(file);
-                    t.sendResponseHeaders(200, 0);
-                    break;
-                default:
-                    t.sendResponseHeaders(405, 0);
-            }
-            t.close();
+    private static void handleGet(Socket socket, String filePath) throws IOException {
+        // check if file exists
+        if (!fileExists(filePath)) {
+            notFound(socket);
+            return;
         }
 
-        // returns true if file exists
-        private boolean fileExists(HttpExchange t, File file) throws IOException {
-            if (!file.exists() || !file.canRead()) {
-                t.sendResponseHeaders(404, 0);
-                return false;
-            }
-            return true;
+        // send contents
+        send(socket, filePath);
+    }
+
+    private static void handlePost(Socket socket, String filePath, String extension, String body) throws IOException {
+        // check if file exists
+        if (!fileExists(filePath)) {
+            notFound(socket);
+            return;
         }
 
-        // sends contents of example.txt
-        private void send(HttpExchange t, Path filePath) throws IOException {
-            byte[] bytes = Files.readAllBytes(filePath);
-            t.sendResponseHeaders(200, bytes.length);
-            OutputStream os = t.getResponseBody();
-            os.write(bytes);
-            os.close();
+        // check if mime is text/plain
+        if (!extension.equals("txt")) {
+            unsupportedMediaType(socket);
+            return;
         }
 
-        // checks MIME type
-        private String mime(Path filePath) throws IOException {
-            return Files.probeContentType(filePath);
-        }
+        // get file
+        FileWriter fileWriter = new FileWriter(filePath, true);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
 
-        // appends a new line to contents of existing file with http body
-        private void editFile(HttpExchange t, String filePath) throws IOException {
+        // write to file
+        bufferedWriter.write(body);
+        bufferedWriter.close();
 
-            // get file
-            FileWriter fileWriter = new FileWriter(filePath, true);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        // send contents
+        send(socket, filePath);
+    }
 
-            // read request body
-            InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
-            BufferedReader br = new BufferedReader(isr);
+    private static void handlePut(Socket socket, String filePath, String body) throws IOException {
+        // create and write to file
+        FileWriter fileWriter = new FileWriter(filePath, false);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        bufferedWriter.write(body);
+        bufferedWriter.close();
 
-            // request body to string
-            int b;
-            StringBuilder buf = new StringBuilder();
-            while ((b = br.read()) != -1) {
-                buf.append((char) b);
-            }
-            br.close();
-            isr.close();
+        // send contents
+        send(socket, filePath);
+    }
 
-            // write to file
-            bufferedWriter.write(buf.toString());
-            bufferedWriter.close();
-        }   
-
-        // overrides existing file or create new file with contents from http body
-        private void overrideFile(HttpExchange t, String filePath) throws IOException {
-            // read request body
-            InputStreamReader isr =  new InputStreamReader(t.getRequestBody(),"utf-8");
-            BufferedReader br = new BufferedReader(isr);
-
-            // request body to string
-            int b;
-            StringBuilder buf = new StringBuilder();
-            while ((b = br.read()) != -1) {
-                buf.append((char) b);
-            }
-            br.close();
-            isr.close();
-            
-            // create and write to file
-            FileWriter fileWriter = new FileWriter(filePath, false);
-            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write(buf.toString());
-            bufferedWriter.close();
-            
+    private static void handleDelete(Socket socket, String filePath) throws IOException {
+        // check if file exists
+        if (!fileExists(filePath)) {
+            notFound(socket);
+            return;
         }
 
         // delete file
-        private void deleteFile(File file) throws IOException {
-            file.delete();
-        }
+        File file = new File(filePath);
+        file.delete();
 
+        // send status code
+        OutputStream out = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(out, true);
+        String header = "HTTP/1.1 200 OK\r\n"+
+                        "\r\n";
+        writer.println(header);
+        out.close();
+        writer.close();
     }
 
+    // returns contents from a filepath as a string
+    private static String readFile(String filePath) throws IOException {
+        File file = new File(filePath);
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String result = "";
+        int c;
+        while ((c = br.read()) != -1) {
+            result += (char)c;
+        }
+        br.close();
+        return result;
+    }
+
+    // sends status code 200 with contents of the file found at file path
+    private static void send(Socket socket, String filePath) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(out, true);
+        String body = readFile(filePath);
+        String header = "HTTP/1.1 200 OK\r\n"+
+                        "Content-Type: text/plain\r\n"+
+                        "Content-Length: " + body.length() + "\r\n"+
+                        "\r\n";
+        writer.println(header + body);
+        out.close();
+        writer.close();
+    }
+
+    // sends status code 404
+    private static void notFound(Socket socket) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(out, true);
+
+        String body = "404 Not Found";
+
+        String header = "HTTP/1.1 404 Not Found\r\n"+
+                        "Content-Type: text/plain\r\n"+
+                        "Content-Length: " + body.length() + "\r\n"+
+                        "\r\n";
+        writer.println(header + body);
+        out.close();
+        writer.close();
+    }
+
+    // sends status code 415
+    private static void unsupportedMediaType(Socket socket) throws IOException {
+        OutputStream out = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(out, true);
+
+        String body = "415 Unsupported Media Type";
+
+        String header = "HTTP/1.1 415 Unsupported Media Type\r\n"+
+                        "Content-Type: text/plain\r\n"+
+                        "Content-Length: " + body.length() + "\r\n"+
+                        "\r\n";
+        writer.println(header + body);
+        out.close();
+        writer.close();
+    }
+
+    // returns true if file exists in the given file path
+    private static boolean fileExists(String filePath) throws IOException {
+        File file = new File(filePath);
+        if (file.exists() && !file.isDirectory()) {
+            return true;
+        }
+        return false;
+    } 
 
 }
